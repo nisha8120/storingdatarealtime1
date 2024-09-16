de084f0bbeee7f0a522a8ca61780b73951c4d6d3
nishameasi <rahmathnisha.a@gmail.com>
2024-09-16 18:21:13 +0530

    nisha

--- app/src/main/java/com/example/storingdatarealtime1/MainActivity.java
+++ app/src/main/java/com/example/storingdatarealtime1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.storingdatarealtime1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 //asdfghjkl
+    FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        fab=findViewById((R.id.fab):
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(createPackageContext())
+            }
         });
+
     }
 }
